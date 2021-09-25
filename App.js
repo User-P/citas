7e0ac4b4dc200029fb9261df3277338c717d0d99
Pedro <pether.1994@gmail.com>
2021-09-25 15:02:01 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import {
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text> Hola mundo</Text>
+    </>
+  );
+};
+
+export default App;
